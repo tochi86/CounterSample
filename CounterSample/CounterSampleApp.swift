291d42cabad57cabdeb48c5491f7cf84c5472d4a
tochi86 <tochi86.ham@gmail.com>
2025-01-02 22:11:56 +0900

    Initial Commit

---
+++ CounterSample/CounterSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterSampleApp.swift
+//  CounterSample
+//
+//  Created by Toshiya Kobayashi on 2025/01/02.
+//
+
+import SwiftUI
+
+@main
+struct CounterSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CounterSampleTests/CounterSampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  CounterSampleTests.swift
+//  CounterSampleTests
+//
+//  Created by Toshiya Kobayashi on 2025/01/02.
+//
+
+import Testing
+@testable import CounterSample
+
+struct CounterSampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
