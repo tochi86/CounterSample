ae329590cb0d98c65dcabf65246c026510025bf1
tochi86 <tochi86.ham@gmail.com>
2025-01-02 22:30:26 +0900

    SwiftFormatを導入

--- CounterSampleTests/CounterSampleTests.swift
+++ CounterSampleTests/CounterSampleTests.swift
@@ -5,13 +5,11 @@
 //  Created by Toshiya Kobayashi on 2025/01/02.
 //
 
-import Testing
 @testable import CounterSample
+import Testing
 
 struct CounterSampleTests {
-
     @Test func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
-
 }
